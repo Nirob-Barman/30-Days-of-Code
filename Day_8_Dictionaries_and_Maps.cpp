65362072d674a2bab2f424fcbc6056a8b6520ef9
Nirob Barman <niroborin111@gmail.com>
2023-09-02 08:47:24 +0600

    Day 08

---
+++ Day_8_Dictionaries_and_Maps.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    map<string, string> phoneBook;
+
+    for (int i = 0; i < n; i++)
+    {
+        string name, phoneNumber;
+        cin >> name >> phoneNumber;
+        phoneBook[name] = phoneNumber;
+    }
+
+    string query;
+    while (cin >> query)
+    {
+        if (phoneBook.find(query) != phoneBook.end())
+        {
+            cout << query << "=" << phoneBook[query] << endl;
+        }
+        else
+        {
+            cout << "Not found" << endl;
+        }
+    }
+    
+    return 0;
+}
