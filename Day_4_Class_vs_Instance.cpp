0f0c6787246bea1c2cbabc313967621116824185
Nirob Barman <niroborin111@gmail.com>
2023-09-02 08:35:30 +0600

    Day 04

---
+++ Day_4_Class_vs_Instance.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Person
+{
+public:
+    int age;
+
+    Person(int initialAge)
+    {
+        if (initialAge < 0)
+        {
+            age = 0;
+            cout << "Age is not valid, setting age to 0." << endl;
+        }
+        else
+        {
+            age = initialAge;
+        }
+    }
+
+    void amIOld()
+    {
+        if (age < 13)
+        {
+            cout << "You are young." << endl;
+        }
+        else if (age >= 13 && age < 18)
+        {
+            cout << "You are a teenager." << endl;
+        }
+        else
+        {
+            cout << "You are old." << endl;
+        }
+    }
+
+    void yearPasses()
+    {
+        age++;
+    }
+};
+
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        int age;
+        cin >> age;
+        Person p(age);
+        p.amIOld();
+
+        for (int j = 0; j < 3; j++)
+        {
+            p.yearPasses();
+        }
+
+        p.amIOld();
+        cout << endl;
+    }
+
+    return 0;
+}
