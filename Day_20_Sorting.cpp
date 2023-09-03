a529a9b3a5202811e1844b68d0904366b401ef7a
Nirob Barman <niroborin111@gmail.com>
2023-09-03 11:35:40 +0600

    Day 20

---
+++ Day_20_Sorting.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int numberOfSwaps = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                swap(a[j], a[j + 1]);
+                numberOfSwaps++;
+            }
+        }
+
+        if (numberOfSwaps == 0)
+        {
+            break;
+        }
+    }
+
+    cout << "Array is sorted in " << numberOfSwaps << " swaps." << endl;
+    cout << "First Element: " << a[0] << endl;
+    cout << "Last Element: " << a[n - 1] << endl;
+    return 0;
+}
